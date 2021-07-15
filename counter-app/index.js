4e22b84ebac46bdc989adba0f4afa14d31239c57
Ushahemba Shir <laleoict@gmail.com>
2021-07-15 16:37:14 +0100

    first commit count prject

---
+++ counter-app/index.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el")
+let saveCount  = document.getElementById("save-count");
+
+let count = 0;
+
+function increment (){
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save (){
+    let countDash = count + " - ";
+    saveEl.textContent += countDash;
+    countEl.textContent = 0;
+    count = 0;
+
+ }
+
+ 
